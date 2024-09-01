b29f44df17d4466e9530ac8d0f3826bfb448a031
PedroSawczuk <pedrosawczuk53@gmail.com>
2024-09-01 01:14:55 -0400

    feat: adicionar verificação e exibir botão para grupo 'professores'

--- core/views.py
+++ core/views.py
@@ -4,5 +4,14 @@ from django.views.generic import *
 class HomePageView(TemplateView):
     template_name = "homePage.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        # Verificar se o usuário está autenticado e pertence ao grupo "professores"
+        context['is_professor'] = user.is_authenticated and user.groups.filter(name='professores').exists()
+
+        return context
+
 class DesenvolvedoresPageView(TemplateView):
     template_name = "desenvolvedoresPage.html"
